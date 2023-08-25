949727265968518846ba258ce2a25180d81a72c8
ManhNH97 <manhnh97@gmail.com>
2023-08-25 23:27:51 +0700

    Latest!

--- Run_CheckAppsTestflight.bat
+++ Run_CheckAppsTestflight.bat
@@ -3,27 +3,30 @@ title "Run Check Apps Beta avaiable on Testflight"
 
 CD /D %~dp0
 set "Result_ErrorLinkTestflight=Result_ErrorLinkTestflight.txt"
-set "stringTestflight=Testflight"
+set "Search_Text=testflight"
 
 set "Result_BetaAppsAvailable=Result_BetaAppsAvailable.md"
 set "Testflight_List=Testflight_List.txt"
 set "Testflight_CheckStatus=Testflight_CheckStatus.py"
+set "RemoveWebsiteError=RemoveWebsiteError.py"
 set "README=README.md"
 
 echo Running Python script...
 python "%Testflight_CheckStatus%"
 echo:
 
-set "found="
-for /f "usebackq delims=" %%a in ("%Result_ErrorLinkTestflight%") do (
-    echo %%a | findstr /C:"%stringTestflight%" >nul && set "found=1"
-)
-
-if defined found (
+findstr %Search_Text% "%Result_ErrorLinkTestflight%" > nul
+if %errorlevel% equ 0 (
     echo Committing changes to Github...
-	git add "%Result_BetaAppsAvailable%" "%Testflight_List%" "%README%"
+	git add "%Result_BetaAppsAvailable%" "%README%"
 	git commit -m "Updated!"
 	git push origin master
+    
+    echo Remove Website Error...
+    python "%RemoveWebsiteError%"
+    git add "%Testflight_List%"
+    git commit -m "Updated list beta apps"
+    git push origin master
 ) else (
     echo Committing changes to Github...
 	git add "%Result_BetaAppsAvailable%" "%README%"
