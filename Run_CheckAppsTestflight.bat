687899f67b3f0bae4c2c4e8fa7981a73f46ca373
ManhNH97 <manhnh97@gmail.com>
2023-08-31 15:47:08 +0700

    Revert "Convert #HASHTAG list to string"
    This reverts commit be55bcd71dd3d1456241b5f98eac339d4d2f6467.

--- Run_CheckAppsTestflight.bat
+++ Run_CheckAppsTestflight.bat
@@ -32,12 +32,12 @@ for /f "usebackq delims=" %%a in ("file.txt") do (
 
 :notfound
 	echo Committing changes to Github...
-	git add "%Result_BetaAppsAvailable%" 
+	git add "%Result_BetaAppsAvailable%" "%README%"
 goto :commitGITHUB
 
 :found
 	echo Committing changes to Github...
-	git add "%Result_BetaAppsAvailable%" 
+	git add "%Result_BetaAppsAvailable%" "%README%"
 
 	echo Remove Website Error...
 	python "%RemoveWebsiteError%"
