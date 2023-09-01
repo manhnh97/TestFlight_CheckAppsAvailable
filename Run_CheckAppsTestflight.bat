23764c3aac135b171a48205338a2cab4353ac0f5
ManhNH97 <manhnh97@gmail.com>
2023-09-01 18:34:25 +0700

    Updated!

--- Run_CheckAppsTestflight.bat
+++ Run_CheckAppsTestflight.bat
@@ -10,43 +10,14 @@ python "%Testflight_CheckStatus%"
 if %errorlevel% neq 0 (
     goto :exit
 )
+
 echo:
-set "Result_ErrorLinkTestflight=Result_ErrorLinkTestflight.txt"
-echo Check if the file "%Result_ErrorLinkTestflight%" exists...
-if not exist "%Result_ErrorLinkTestflight%" (
-    goto :exit
-)
-echo:
-echo Check if the file "%Result_ErrorLinkTestflight%" contains the string "testflight"
-set "Result_BetaAppsAvailable=Result_BetaAppsAvailable.md"
-set "Testflight_List=Testflight_List.txt"
-set "RemoveWebsiteError=RemoveWebsiteError.py"
-set "README=README.md"
-set "found=false"
-for /f "usebackq delims=" %%a in ("file.txt") do (
-    if "%%a"=="testflight" (
-        set "found=true"
-        goto :found
-    )
-)
 
-:notfound
-	echo Committing changes to Github...
-	git add "%Result_BetaAppsAvailable%" "%README%"
-goto :commitGITHUB
-
-:found
-	echo Committing changes to Github...
-	git add "%Result_BetaAppsAvailable%" "%README%"
-
-	echo Remove Website Error...
-	python "%RemoveWebsiteError%"
-	git add "%Testflight_List%"
-	git commit -m "Updated list beta apps"
-	git push origin master
 goto :commitGITHUB
 
 :commitGITHUB
+	set "Result_BetaAppsAvailable=Result_BetaAppsAvailable.md"
+	git add "%Result_BetaAppsAvailable%"
 	git commit -m "Updated!"
 	git push origin master
 goto :exit
