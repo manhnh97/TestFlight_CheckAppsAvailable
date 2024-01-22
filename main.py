c131bb1a1cdf9d37e68b6a5716c91db9d2ca25e4
ManhNH97 <manhnh97@gmail.com>
2024-01-22 12:57:07 +0700

    Get result full

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ from requests.exceptions import ConnectTimeout
 from fake_useragent import UserAgent
 from datetime import datetime
 
+TXT_TESTFLIGHT_LIST = "Testflight_List.txt"
+TXT_RESULT_AVAILABLE_BETA_APPS = "Result_Available_BetaApps.md"
+TXT_RESULT_FULL_BETA_APPS = "Result_Full_BetaApps.md"
+TXT_RESULT_ERROR_BETA_APPS = "Result_Available_BetaApps.md"
+
 def fetch_beta_apps_info():
-    with open("Testflight_List.txt", 'r', encoding='utf-8') as txt_testflight_list_file,\
-            open("Result_BetaAppsAvailable.md", 'w', encoding='utf-8') as txt_result_available_testflight_file,\
-            open("Result_ErrorLinkTestflight.txt", 'w', encoding='utf-8') as txt_result_error_link_testflight_file:
+    with open(TXT_TESTFLIGHT_LIST, 'r', encoding='utf-8') as txt_testflight_list_file,\
+            open(TXT_RESULT_AVAILABLE_BETA_APPS, 'w', encoding='utf-8') as txt_result_available_testflight_file,\
+            open(TXT_RESULT_FULL_BETA_APPS, 'w', encoding='utf-8') as txt_result_full_testflight_file,\
+            open(TXT_RESULT_ERROR_BETA_APPS, 'w', encoding='utf-8') as txt_result_error_link_testflight_file:
         
         try:
             user_agent = UserAgent()
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '}
-            
+            pattern_Available = r'To join the\s(.*?)\sbeta'
+            pattern_Full = r'Join the\s(.*?)\sbeta'
+                        
             session = requests.Session()
             
             urls = list(set(txt_testflight_list_file.read().split()))
@@ -35,15 +43,23 @@ def fetch_beta_apps_info():
                     urls.append(url_testflight)
                     
                 if r.status_code == 200:
-                    soup_text = bs(r.text, 'html.parser').get_text()
+                    soup_text = bs(r.text, 'html.parser')
+                    beta_status_element = soup_text.find(class_='beta-status')
+                    first_span = beta_status_element.find('span')
+                    span_text = first_span.get_text(strip=True)
                     
-                    text_matches = re.search(r'To join the\s(.*?)\sbeta', soup_text, re.IGNORECASE)
-                    if text_matches:
+                    if re.search(pattern_Available, span_text, re.IGNORECASE):
+                        text_matches = re.search(pattern_Available, span_text, re.IGNORECASE)
                         textname_between_tothe_and_beta = text_matches.group(1).strip()
                         name = ''.join(textname_between_tothe_and_beta).replace('|', '-')
                         hashtags = re.findall(r"\b\w+\b", name)
                         hashtag = " ".join(["#" + hashtag.upper() for hashtag in hashtags])
                         txt_result_available_testflight_file.write(f"| **{name.strip()}** | {hashtag}<br />{url_testflight} |\n")
+                    elif "This beta is full." == span_text:
+                        title_text = soup_text.find('title').getText()
+                        text_matches = re.search(pattern_Full, title_text, re.IGNORECASE)
+                        textname_between_join_and_beta = text_matches.group(1).strip()
+                        txt_result_full_testflight_file.write(f"{textname_between_join_and_beta} => {url_testflight}")
                 else:
                     txt_result_error_link_testflight_file.write(f"{url_testflight}\n")
         except AttributeError:
@@ -52,7 +68,7 @@ def fetch_beta_apps_info():
             session.close()
 
 def sort_and_update_results():
-    with open("Result_BetaAppsAvailable.md", "r", encoding="utf-8") as txt_result_available_testflight_file:
+    with open(TXT_RESULT_AVAILABLE_BETA_APPS, "r", encoding="utf-8") as txt_result_available_testflight_file:
         contents = txt_result_available_testflight_file.readlines()
 
     def extract_text_within_brackets(line):
@@ -61,20 +77,20 @@ def sort_and_update_results():
 
     contents.sort(key=lambda x: extract_text_within_brackets(x))
 
-    with open("Result_BetaAppsAvailable.md", "w", encoding="utf-8") as txt_result_available_testflight_file:
+    with open(TXT_RESULT_AVAILABLE_BETA_APPS, "w", encoding="utf-8") as txt_result_available_testflight_file:
         txt_result_available_testflight_file.write(f"# Beta Apps are available\t[{datetime.now().strftime('%d/%m/%Y %I:%M %p')}]\n")
         txt_result_available_testflight_file.write('| Name | #HASHTAG |\n| --- | --- | \n')
         txt_result_available_testflight_file.writelines(contents)
 
 def update_testflight_list():
-    with open("Testflight_List.txt", 'r') as f1, open("Result_ErrorLinkTestflight.txt", 'r') as f2:
+    with open(TXT_TESTFLIGHT_LIST, 'r') as f1, open(TXT_RESULT_ERROR_BETA_APPS, 'r') as f2:
         lines_f1 = f1.read().splitlines()
         lines_f2 = f2.read().splitlines()
 
     unique_lines_f1 = list(set(lines_f1))
     updated_lines_f1 = [line for line in unique_lines_f1 if line not in lines_f2]
 
-    with open("Testflight_List.txt", 'w') as f1:
+    with open(TXT_TESTFLIGHT_LIST, 'w') as f1:
         f1.write('\n'.join(updated_lines_f1))
 
 if __name__ == "__main__":
