cf95f780a1a89d8b3a9c4013ca1f68da518b2a69
ManhNH97 <manhnh97@gmail.com>
2024-01-23 10:00:51 +0700

    Updated!!!

--- Run_CheckAppsTestflight.bat
+++ Run_CheckAppsTestflight.bat
@@ -4,18 +4,12 @@ title Check Apps Beta available on Testflight
 CD /D %~dp0
 
 git pull origin master    
-goto :pythonScript
 
-:pythonScript
-set "Testflight_CheckStatus=main.py"
-echo Run "%Testflight_CheckStatus%" script...
-python "%Testflight_CheckStatus%"
-goto :commitGITHUB
+python "main.py"
 
-:commitGITHUB
-git add "Result_BetaAppsAvailable.md" "Testflight_List.txt"
+timeout 3
+git add "Testflight_List.txt" "Result_Available_BetaApps.md" "Result_Full_BetaApps.md"
 git commit -m "Updated!"
 git push origin master
 
-rundll32 user32.dll,MessageBeep
 exit /B
